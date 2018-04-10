423afde757c6813b0cd6d747e55b2b8fa73aa2ab
jkoul <jkoulouris@hotmail.co.uk>
2018-04-10 12:46:59 +0100

    introduced play method which can act on environment

--- flappybird/run.py
+++ flappybird/run.py
@@ -1,9 +1,13 @@
-import logging
-import os, sys
-
 import gym
-from gym.wrappers import Monitor
 import gym_ple
+import os, sys
+import logging
+import numpy as np
+
+from ple import PLE
+from gym.wrappers import Monitor
+from ple.games.flappybird import FlappyBird
+
 
 # The world's simplest agent!
 class RandomAgent(object):
@@ -13,51 +17,74 @@ class RandomAgent(object):
     def act(self, observation, reward, done):
         return self.action_space.sample()
 
-if __name__ == '__main__':
-    # You can optionally set up the logger. Also fine to set the level
-    # to logging.DEBUG or logging.WARN if you want to change the
-    # amount of output.
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
+def visualization_test():
+    if __name__ == '__main__':
+        # You can optionally set up the logger. Also fine to set the level
+        # to logging.DEBUG or logging.WARN if you want to change the
+        # amount of output.
+        logger = logging.getLogger()
+        logger.setLevel(logging.INFO)
 
-    env = gym.make('FlappyBird-v0' if len(sys.argv)<2 else sys.argv[1])
+        env = gym.make('FlappyBird-v0' if len(sys.argv)<2 else sys.argv[1])
 
-    # You provide the directory to write to (can be an existing
-    # directory, including one with existing data -- all monitor files
-    # will be namespaced). You can also dump to a tempdir if you'd
-    # like: tempfile.mkdtemp().
-    outdir = '/tmp/random-agent-results'
+        # You provide the directory to write to (can be an existing
+        # directory, including one with existing data -- all monitor files
+        # will be namespaced). You can also dump to a tempdir if you'd
+        # like: tempfile.mkdtemp().
+        outdir = '/tmp/random-agent-results'
 
-    #env = Monitor(env, directory=outdir, force=True)
-    env.render()
+        #env = Monitor(env, directory=outdir, force=True)
+        env.render()
 
-    # This declaration must go *after* the monitor call, since the
-    # monitor's seeding creates a new action_space instance with the
-    # appropriate pseudorandom number generator.
-    env.seed(0)
-    agent = RandomAgent(env.action_space)
+        # This declaration must go *after* the monitor call, since the
+        # monitor's seeding creates a new action_space instance with the
+        # appropriate pseudorandom number generator.
+        env.seed(0)
+        agent = RandomAgent(env.action_space)
 
-    episode_count = 100
-    reward = 0
-    done = False
+        episode_count = 100
+        reward = 0
+        done = False
 
-    for i in range(episode_count):
-        ob = env.reset()
+        for i in range(episode_count):
+            ob = env.reset()
 
-        while True:
-            action = agent.act(ob, reward, done)
-            ob, reward, done, _ = env.step(action)
-            env.render()
-            if done:
-                break
-            # Note there's no env.render() here. But the environment still can open window and
-            # render if asked by env.monitor: it calls env.render('rgb_array') to record video.
-            # Video is not recorded every episode, see capped_cubic_video_schedule for details.
+            while True:
+                action = agent.act(ob, reward, done)
+                ob, reward, done, _ = env.step(action)
+                env.render()
+                #print(env)
+                if done:
+                    break
+                # Note there's no env.render() here. But the environment still can open window and
+                # render if asked by env.monitor: it calls env.render('rgb_array') to record video.
+                # Video is not recorded every episode, see capped_cubic_video_schedule for details.
 
-    # Dump result info to disk
-    env.close()
+        # Dump result info to disk
+        env.close()
 
-    # Upload to the scoreboard. We could also do this from another
-    # process if we wanted.
-    logger.info("Successfully ran RandomAgent. Now trying to upload results to the scoreboard. If it breaks, you can always just try re-uploading the same results.")
-    gym.upload(outdir)
+        # Upload to the scoreboard. We could also do this from another
+        # process if we wanted.
+        logger.info("Successfully ran RandomAgent. Now trying to upload results to the scoreboard. If it breaks, you can always just try re-uploading the same results.")
+        gym.upload(outdir)
+
+def process_state(state):
+        return np.array([ state.values() ])
+
+def play(episodes=10):
+    # Initialize game with wrapper object
+    game = FlappyBird()
+    p = PLE(game, display_screen=True, state_preprocessor=process_state)
+    p.init()
+
+    # Run given number of episodes
+    for _ in range(episodes):
+        p.reset_game()
+        while not p.game_over():
+            state = p.getGameState()
+            action = np.random.choice(p.getActionSet())
+            #action = agent.pickAction(reward, state)
+            reward = p.act(action)
+            print(state,action,reward)
+
+play()
